9a89527f5f7fc88d656a0d5a9fd4d67a07ffb572
Facundo Robert <robert.facundo@hotmail.com>
2025-03-05 15:49:30 -0300

    again

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,16 +3,20 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   build: {
-    outDir: 'frontend/dist', // Configura el directorio de salida de la compilación
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: 'index.html',  // Ruta relativa al archivo 'index.html' ahora que está en la raíz
+    },
   },
   plugins: [react()],
   server: {
-    port: 3001, 
+    port: 3001,
   },
   test: {
     reporters: ['verbose'],
-    globals: true, 
-    environment: 'jsdom', 
+    globals: true,
+    environment: 'jsdom',
     setupFiles: './setupTests.js'
   },
 })
