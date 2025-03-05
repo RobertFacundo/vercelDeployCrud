442f20d18003b0708fbf5802d3139d57e54b0dba
Facundo Robert <robert.facundo@hotmail.com>
2025-03-04 22:52:05 -0300

    variables de entorno

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,8 @@ import mongoose, { connect } from 'mongoose';
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/crudDB');
+        const MONGO_URI = process.env.MONGO_URI
+        await mongoose.connect(MONGO_URI);
         console.log('Conexion exitosa a la base de datos')
     } catch (err) {
         console.error('Error al conectar a la base de datos', err)
